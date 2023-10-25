11dcc5f27b6b5e6c42716c0492794b77cc76a8fe
Alexandre Muraviov <amuraviov@globalia.ca>
2023-10-25 06:53:10 -0400

    user registration and login, jwt token

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,48 @@
 const ApiError = require('../error/apiError.js')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
+
+const generateJWT = (id, email, role) => {
+    return jwt.sign({id, email, role}, process.env.SECRET_JWT_KEY, {expiresIn: '24h'})
+}
 
 class UserController {
-    async registration(req, res) {
-        
+    async registration(req, res, next) {
+        const {email, password, role} = req.body
+
+        if (!email || !password) {
+            return next(ApiError.badRequest('Wrong / Missing email or password'))
+        }
+
+        const candidate = await User.findOne({where: {email}})
+        if (candidate) {
+            return next(ApiError.badRequest('This email is already registered'))
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJWT(user.id, email, role)
+
+        return res.json({token})
     }
 
-    async login(req, res) {
+    async login(req, res, next) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if (!user) {
+            return next(ApiError.internal('User not found or password is wrong'))
+        }
 
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internal('User not found or password is wrong'))
+        }
+
+        const token = generateJWT(user.id, user.email, user.role)
+
+        return res.json({token})
     }
 
     async check(req, res, next) {
