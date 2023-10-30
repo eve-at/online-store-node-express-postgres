1d5f0be92bc2d69dc1eee3acf2d6e590b61dce37
Alexandre Muraviov <amuraviov@globalia.ca>
2023-10-30 10:51:44 -0400

    type bar

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -5,7 +5,8 @@ export default class DeviceStore {
         this._types = [
             {id: 1, name: 'Fridges'},
             {id: 2, name: 'TVs'},
-            {id: 3, name: 'Smartphones'}
+            {id: 3, name: 'Smartphones'},
+            {id: 4, name: 'Laptops'}
         ]
         this._brands = [
             {id: 1, name: 'Sony'},
@@ -17,6 +18,7 @@ export default class DeviceStore {
             {id: 2, name: 'a51', 'price': 15000, 'rating': 0, 'typeId': 3, 'brandId': 2, 'img': "88458511-8d00-4d5e-a29a-49e7b04c413f.jpg"},
             {id: 3, name: 'Samsung 4 door fridge', 'price': 12000, 'rating': 0, 'typeId': 1, 'brandId': 2, 'img': "a7f5664e-42b3-470d-921b-b203dcbe78fe.jpg"}
         ]
+        this._selectedType = {}
         makeAutoObservable(this)
     }    
 
@@ -32,6 +34,10 @@ export default class DeviceStore {
         this._devices = devices
     }
 
+    setSelectedType(type) {
+        this._selectedType = type
+    }
+
     get types() {
         return this._types
     }
@@ -42,5 +48,9 @@ export default class DeviceStore {
 
     get devices() {
         return this._devices
+    }
+
+    get selectedType() {
+        return this._selectedType
     }
 }
